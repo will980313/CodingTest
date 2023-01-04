69aad861f645d03976ab4aed210141b48b113283
이주언 <42673860+will980313@users.noreply.github.com>
2023-01-05 00:19:47 +0900

    [Bronze II] Title: 저항, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,5 @@
+col_list =['black','brown','red','orange','yellow','green','blue','violet','grey','white']
+a = input()
+b = input()
+c = input()
+print(int(str(col_list.index(a))+str(col_list.index(b)))*(10**col_list.index(c)))
